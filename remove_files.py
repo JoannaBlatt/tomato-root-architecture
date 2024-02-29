33d0afa029ddba9eef70547ec701407057efe446
JoannaBlatt <110856717+JoannaBlatt@users.noreply.github.com>
2024-02-29 00:38:24 +0000

    started remove files functions

---
+++ remove_files.py
@@ -0,0 +1,15 @@
+"""This file has a function to remove all the files in a directory.
+It has a second function to go through each directory to remove the files.
+
+"""
+import os
+from constants import *
+
+def remove_files(directory):
+    """removes all files from a given directory
+    """
+    for fname in directory:
+        os.remove(directory+"/"+fname)
+def remove_all_files():
+    for directory in NECESSARY_DIR_LIST:
+        print(directory)

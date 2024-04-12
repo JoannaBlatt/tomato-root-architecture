c135d99d165493bd9a610c596ce7f4275bcbbc58
JoannaBlatt <110856717+JoannaBlatt@users.noreply.github.com>
2024-04-11 19:26:56 -0500

    Currently can save to server and run script

--- upload.php
+++ upload.php
@@ -9,10 +9,6 @@ const uploadInput = document.getElementById("testingfileuplaod");
         </head>
 <body>
 <?php
-echo "hello<br>";
-
-/* Get the name of the uploaded file */
-$filename = $_FILES['file']['name'];
 
 echo "<br>Code for vardump section begin:<br>";
 function var_dump_pre($mixed = null) {
@@ -39,27 +35,11 @@ echo getRequestData("testingfileupload");
 echo "<br>end of vardump code section<br><br>";
 
 /* Choose where to save the uploaded file */
-$location = "/home/dh_an3skk/arjun-chandrasekhar-teaching.com/temp";
+$location = "/home/dh_an3skk/arjun-chandrasekhar-teaching.com/tomato/temp/bk_".$_FILES["testingfileupload"]["name"];  #This saves the file with name bk -- this should be changed later
 echo "<BR>loacation: ".$location."<br>";
 echo "<BR>";
-echo "file name: ".$filename."<br>";
-echo "<BR>";
 
-//echo "<BR>testing code from chelidze dot givia at gmail dot com on php page<BR>";
-//$file = $_FILES["testingfileupload"];
-//$filePath = 'temp/'.generateDir(10).'/'.$file["name"];
-// Make the directory first or else it will not proceed with the upload.
-//mkdir($filePath);
-//move_uploaded_file($file["tmp_name"],  $filePath);
 
-/* Save the uploaded file to the local filesystem */
-/*if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
-  echo 'Success'; 
-} else { 
-  echo 'Failure'; 
-}*/
-/*echo $_REQUEST['testingfileupload']*/
-//if ( move_uploaded_file($_FILES['testingfileupload']["tmp_name"], $location) ) { 
 if ( copy($_FILES['testingfileupload']["tmp_name"], $location) ) { 
 
   echo 'Success'; 

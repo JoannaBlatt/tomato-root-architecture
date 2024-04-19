a643022f22e7078677cb70607da4b3e110bd2bb3
ZekkieMcCormick <mccormicg@southwestern.edu>
2024-04-19 09:40:43 -0500

    changes from wed

--- test_passing_var.php
+++ test_passing_var.php
@@ -1,4 +1,8 @@
 <?php
 $receivedData = $_GET['myData'];
 echo "Received data from JavaScript: " . $receivedData;
+
+  $html = file_get_contents('upload.html');
+  echo str_replace('passBack()', "passBack('{$receivedData}')", $html);
+
 ?>

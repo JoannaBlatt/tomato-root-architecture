ad59beccf14912fe1a11be8ec8588c4d1c2f3ad6
JoannaBlatt <110856717+JoannaBlatt@users.noreply.github.com>
2024-04-25 23:59:29 -0500

    updated comments

--- backend_plant_architecture/makeDir.py
+++ backend_plant_architecture/makeDir.py
@@ -3,15 +3,17 @@ import sys
 
 def main():
     #print ('argument list', sys.argv)
-    initialPath = sys.argv[1]
+    initialPath = sys.argv[1]   # grabs the initial sessionID/fileName variable which is the name of the base directory
     mainDirectory = initialPath.rstrip(".csv")
     
+    # creates full paths for all directories using passed sessionID and fileName variable
     STATISTICS_DIR = '%s/statistics' % mainDirectory
     RECONSTRUCTIONS_DIR = '%s/arbor-reconstructions' % mainDirectory
     PARETO_FRONTS_DIR = '%s/pareto-fronts' % mainDirectory
     NULL_MODELS_DIR = '%s/null-models' % mainDirectory
     FRONT_DRAWINGS_DIR = '%s/pareto-front-drawings' % mainDirectory
 
+    # creates the directories and parent directories if needed
     pathlib.Path(RECONSTRUCTIONS_DIR).mkdir(parents=True, exist_ok=True) 
     pathlib.Path(PARETO_FRONTS_DIR).mkdir(parents=True, exist_ok=True) 
     pathlib.Path(FRONT_DRAWINGS_DIR).mkdir(parents=True, exist_ok=True) 
@@ -19,7 +21,7 @@ def main():
     pathlib.Path(NULL_MODELS_DIR).mkdir(parents=True, exist_ok=True) 
 
 
-
+    # makes directories of all original paths
 """
 #pathlib.Path(ORIGINAL_ROOT_NODES_DIR).mkdir(parents=True, exist_ok=True) 
 #pathlib.Path(CLEANED_ROOT_NODES_DIR).mkdir(parents=True, exist_ok=True) 
